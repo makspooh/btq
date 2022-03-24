2dfa3a71259631100584a8c9b72ce3f471e82057
Maksym Pokhylyi <maksimpooh@gmail.com>
2022-03-24 15:03:41 +0200

    init firebase, add deps, create basic architecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ROOT } from './utils/constants/routes';
+
+import Main from './components/pages/Main';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={ROOT}
+            element={<Main />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/utils/constants/routes.js
@@ -0,0 +1,5 @@
+const ROOT = '/';
+
+export {
+    ROOT
+};
